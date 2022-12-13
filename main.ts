afae01efa7b913de894e4ee8f1916556f239573e
safije22 <120473451+safije22@users.noreply.github.com>
2022-12-13 14:40:50 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    music.startMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once)
+})
+input.onButtonPressed(Button.B, function () {
+    music.startMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once)
+})
